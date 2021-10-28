1279fc8926a70cbad2a1af077e641ad0c0e77218
TheKnightOfDarkness <gameroomca@gmail.com>
2021-10-27 22:37:02 -0600

    Db-Teachers

---
+++ database/migrations/2021_10_28_024200_create__teachers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_teachers', function (Blueprint $table) {
+            
+            $table->id('Cedula');
+            $table->timestamps();
+            $table->String('Nombre');
+            $table->String('Primer Apellido');
+            $table->string('Segundo Apellido');
+            $table->date('Fecha de Nacimiento');
+            $table->integer('Edad');
+            $table->string('Especialidad');
+            $table->string('Correo Electronico');
+            $table->string('Sexo Del Docente');
+            $table->Integer('Numero De Contacto');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_teachers');
+    }
+}
