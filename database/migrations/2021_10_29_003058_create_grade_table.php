0da592dcc07f49a3424142933177038191fbeddb
TheKnightOfDarkness <gameroomca@gmail.com>
2021-10-28 18:46:38 -0600

    tabla de grados

---
+++ database/migrations/2021_10_29_003058_create_grade_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGradeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('grade', function (Blueprint $table) {
+            $table->id();
+            $table->id('grade_id');
+            $table->integer('grade');
+            $table->integer('section');
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('grade');
+    }
+}
