b4622a79b67f38806a49849f257424e58d516aa5
LuisVicente123 <luisarandavi@gmail.com>
2021-10-29 11:58:57 -0600

    cambiar datos

--- database/migrations/2021_10_29_020518_create_students_table.php
+++ database/migrations/2021_10_29_020518_create_students_table.php
@@ -15,11 +15,19 @@ class CreateStudentsTable extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->integer('id');
+            $table->integer('student_id');
             $table->string('name');
-            $table->integer('grade');
             $table->string('nationality');
+            $table->integer('grade');
+            $table->set('priority', ['1', '2', '3', '4', '5'])
+            $table->decimal('salarial constance');
+            $table->string('legal_guardian_name');
+            $table->integer('legal_guardian_id');
+            $table->string('place_residence');
+            $table->string('phone_number');
+            $table->set('socioeconomic_status', ['extreme_poverty', 'poverty', 'vulnerability', 'not_poor']);
             $table->integer('total_family_group');
+            $table->integer('family_member_total');
             $table->decimal('total_per_capita');
             $table->boolean('clasification');
             $table->timestamps();
