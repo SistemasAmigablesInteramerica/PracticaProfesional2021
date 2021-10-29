4849fdfa22e243697426d471d31c2661e076a5d1
LuisVicente123 <luisarandavi@gmail.com>
2021-10-29 08:48:51 -0600

    datos iniciales

---
+++ database/migrations/2021_10_29_144257_create_subject_grade_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubjectGradeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subject_grade', function (Blueprint $table) {
+            $table->id();
+            $table->integer('grade_id');
+            $table->integer('subject_id');
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+
+            $table->foreign('grade_id')->references('grade_id')->on('grade');
+            $table->foreign('subject_id')->references('subject_id')->on('subjects');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subject_grade');
+    }
+}
