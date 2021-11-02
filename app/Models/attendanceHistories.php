86ff96ad2c6c4476e46de8ff86c1e7f7e268d876
LuisVicente123 <luisarandavi@gmail.com>
2021-11-02 08:07:03 -0600

    corregir errores

--- app/Models/attendanceHistories.php
+++ app/Models/attendanceHistories.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class AttendanceHistories extends Model
 {
     protected $table = 'attendance_histories';
-    protected $fillable = ['date', 'check_in', 'check_out', 'student_id', 'grade_subject_teacher_id', 'teacher_id', 'attended']
+    protected $fillable = ['date', 'check_in', 'check_out', 'student_id', 'grade_subject_teacher_id', 'teacher_id', 'attended'];
     use HasFactory;
 }   
 
