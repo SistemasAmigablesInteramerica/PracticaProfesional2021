c6ca48871ab1df94652e2400f17713f89308ef76
LuisVicente123 <luisarandavi@gmail.com>
2021-11-02 07:54:25 -0600

    corregir errores

--- app/Models/attendanceHistories.php
+++ app/Models/attendanceHistories.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class attendanceHistories extends Model
+class AttendanceHistories extends Model
 {
     protected $table = 'attendance_histories';
     protected $fillable = ['date', 'check_in', 'check_out', 'student_id', 'grade_subject_teacher_id', 'teacher_id', 'attended']
@@ -14,12 +14,12 @@ class attendanceHistories extends Model
 
 public function student()
 {
-    return $this->hasMany(students::class, 'student_id', 'id');
+    return $this->belongsTo(students::class, 'student_id', 'id');
 }
 
 public function teacher()
 {
-    return $this->hasMany(teachers::class, 'teacher_id', 'id');
+    return $this->belongsTo(teachers::class, 'teacher_id', 'id');
 }
 
 public function subject_teacher()
--- app/Models/gradeSubjectStudents.php
+++ app/Models/gradeSubjectStudents.php
@@ -5,17 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class gradeSubjectStudents extends Model
+class GradeSubjectStudents extends Model
 {
     protected $table = 'grade_subject_students';
     protected $fillable = ['subject_grade_id', 'student_id', 'year'];
     use HasFactory;
 }
-public function subjectGrade()
+public function SubjectGrade()
 {
-    return $this->belongsTo(subject_grades::class, 'subject_grade_id', 'id');
+    return $this->belongsTo(SubjectGrades::class, 'subject_grade_id', 'id');
 }
-public function student()
+public function Student()
 {
-    return $this->BelongsTo(students::class, 'student_id', 'id');
+    return $this->belongsTo(Students::class, 'student_id', 'id');
 }
