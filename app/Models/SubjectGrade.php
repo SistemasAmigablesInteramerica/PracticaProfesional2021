0f882d2c1073e3afa9f84d9df1c776b772a33528
LuisVicente123 <luisarandavi@gmail.com>
2021-11-02 08:34:28 -0600

    corregir error

---
+++ app/Models/SubjectGrade.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SubjectGrade extends Model
+{
+    protected $table = 'subject_grades';
+    protected $fillable = ['grade_id', 'subject_id']; 
+    use HasFactory;
+}
+
+    public function grade()
+    {
+    return $this->belongsTo(Grade::class, 'grade_id', 'id');
+    }
+        public function subject()
+    {   
+    return $this->belongsTo(Subject::class, 'subject_id', 'id');
+    }
+    public function gradeSubjectStudent()
+    {   
+    return $this->hasMany(GradeSubjectStudent::class, 'subject_grade_id', 'id');  
+    }
+    public function subjectTeacher()
+    {
+    return $this->hasMany(SubjectTeacher::class, 'subject_grade_id', 'id');
+    }
