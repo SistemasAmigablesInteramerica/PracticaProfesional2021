bfab08a2ace35359d31ef0ff66f9f5369634225a
TheKnightOfDarkness <gameroomca@gmail.com>
2021-11-03 17:41:07 -0600

    agregar botones

--- resources/views/insert-subjectinfo.blade.php
+++ resources/views/insert-subjectinfo.blade.php
@@ -19,7 +19,59 @@
 
     </head>
     <body>
-
+    <header class="header-area header-sticky">
+      <div class="container">
+          <div class="row">
+              <div class="col-12">
+                  <nav class="main-nav">
+                      <!-- ***** Logo Start ***** -->
+                      <a href="/" class="logo">
+                          Fullpass Students
+                      </a>
+                      <!-- ***** Logo End ***** -->
+                      <!-- ***** Menu Start ***** -->
+                      <ul class="nav">
+                          <li class="has-sub">  
+                              <a href="javascript:void(0)">Expediente estudiantes</a>
+                              <ul class="sub-menu">
+                                  <li><a href="/insert-studentsinfo">Agregar estudiante</a></li>
+                                  <li><a href="/view-studentsinfo">Ver expedientes</a></li>
+                                  <li><a href="/insert-studentrelativesinfo">Agregar Familiares</a></li>
+                                  <li><a href="/view-studentrelativesinfo">Ver Familiares</a></li>
+                              </ul>
+                          </li>
+                          <li class="has-sub">  
+                              <a href="javascript:void(0)">Expediente Profesores</a>
+                              <ul class="sub-menu">
+                                  <li><a href="/insert-teacherinfo">Agregar profesores</a></li>
+                                  <li><a href="/view-teacherinfo">Ver profesores</a></li>
+                              </ul>
+                          </li>
+                          <li class="has-sub">  
+                              <a href="javascript:void(0)">Materias</a>
+                              <ul class="sub-menu">
+                                  <li><a href="/insert-subjectinfo">Agregar Materias</a></li>
+                                  <li><a href="/view-subjectinfo">Ver Materias</a></li>
+                              </ul>
+                          </li>
+                          <li class="has-sub">  
+                              <a href="javascript:void(0)">Secciones</a>
+                              <ul class="sub-menu">
+                                  <li><a href="/insert-gradeinfo">Agregar sección</a></li>
+                                  <li><a href="/view-gradeinfo">Ver secciones</a></li>
+                              </ul>
+                          </li>
+                          <li><a href="/meetings">Meetings</a></li>
+                      </ul>        
+                      <a class="menu-trigger">
+                          <span>Menu</span>
+                      </a>
+                      <!-- ***** Menu End ***** -->
+                  </nav>
+              </div>
+          </div>
+      </div>
+  </header>
     <header class="header-area header-sticky">
       <div class="container">
           <div class="row">
---
+++ resources/views/insert-teachersubjectinfo.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header class="header-area header-sticky">
+      <div class="container">
+          <div class="row">
+              <div class="col-12">
+                  <nav class="main-nav">
+                      <!-- ***** Logo Start ***** -->
+                      <a href="/" class="logo">
+                          Fullpass Students
+                      </a>
+                      <!-- ***** Logo End ***** -->
+                      <!-- ***** Menu Start ***** -->
+                      <ul class="nav">
+                          <li class="has-sub">  
+                              <a href="javascript:void(0)">Expediente estudiantes</a>
+                              <ul class="sub-menu">
+                                  <li><a href="/insert-studentsinfo">Agregar estudiante</a></li>
+                                  <li><a href="/view-studentsinfo">Ver expedientes</a></li>
+                                  <li><a href="/insert-studentrelativesinfo">Agregar Familiares</a></li>
+                                  <li><a href="/view-studentrelativesinfo">Ver Familiares</a></li>
+                              </ul>
+                          </li>
+                          <li class="has-sub">  
+                              <a href="javascript:void(0)">Expediente Profesores</a>
+                              <ul class="sub-menu">
+                                  <li><a href="/insert-teacherinfo">Agregar profesores</a></li>
+                                  <li><a href="/view-teacherinfo">Ver profesores</a></li>
+                              </ul>
+                          </li>
+                          <li class="has-sub">  
+                              <a href="javascript:void(0)">Materias</a>
+                              <ul class="sub-menu">
+                                  <li><a href="/insert-subjectinfo">Agregar Materias</a></li>
+                                  <li><a href="/view-subjectinfo">Ver Materias</a></li>
+                              </ul>
+                          </li>
+                          <li class="has-sub">  
+                              <a href="javascript:void(0)">Secciones</a>
+                              <ul class="sub-menu">
+                                  <li><a href="/insert-gradeinfo">Agregar sección</a></li>
+                                  <li><a href="/view-gradeinfo">Ver secciones</a></li>
+                              </ul>
+                          </li>
+                          <li><a href="/meetings">Meetings</a></li>
+                      </ul>        
+                      <a class="menu-trigger">
+                          <span>Menu</span>
+                      </a>
+                      <!-- ***** Menu End ***** -->
+                  </nav>
+              </div>
+          </div>
+      </div>
+  </header>
+    
+</body>
+</html>
