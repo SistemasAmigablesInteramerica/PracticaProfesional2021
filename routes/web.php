34e594b989e4c698344adf51125c72bba1ab6b5f
LuisVicente123 <luisarandavi@gmail.com>
2021-11-09 12:06:32 -0600

    corregir errores

--- routes/web.php
+++ routes/web.php
@@ -14,55 +14,36 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [App\Http\Controllers\IndexController::class, 'create'])->name('index');
-});
-
-Route::get('/registro-de-estudiantes', [App\Http\Controllers\StudentController::class, 'create'])->name('insert-studentsinfo');
-});
 
 Route::get('/registro-de-secciones', [App\Http\Controllers\GradeController::class, 'create'])->name('insert-gradeinfo');
-});
 
 Route::get('/registro-de-profesores', [App\Http\Controllers\TeacherController::class, 'create'])->name('insert-teacherinfo');
-});
 
 Route::get('/registro-de-materias', [App\Http\Controllers\SubjectController::class, 'create'])->name('insert-subjectinfo');
-});
 
 Route::get('/registro-de-familiares', [App\Http\Controllers\StudentRelativeController::class, 'create'])->name('insert-studentrelativesinfo');
-});
 
 Route::get('/asignar-materia-grado', [App\Http\Controllers\SubjectGradeController::class, 'create'])->name('insert-subjectgradeinfo');
-});
 
 Route::get('/asignar-materia-profesor', [App\Http\Controllers\SubjectTeacherController::class, 'create'])->name('insert-subjectteacherinfo');
-});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'create'])->name('home');
-});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'create'])->name('home');
-});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'create'])->name('home');
-});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'create'])->name('home');
-});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'create'])->name('home');
-});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'create'])->name('home');
-});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'create'])->name('home');
-});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'create'])->name('home');
-});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'create'])->name('home');
-});
 
 Auth::routes();
 
