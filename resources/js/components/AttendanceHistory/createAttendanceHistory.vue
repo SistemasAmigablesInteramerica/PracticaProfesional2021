fc2e22e1cbec743f1513aca5714a9d2d8791905b
LuisVicente123 <luisarandavi@gmail.com>
2021-11-22 21:46:29 -0600

    corregir errores

--- resources/js/components/AttendanceHistory/createAttendanceHistory.vue
+++ resources/js/components/AttendanceHistory/createAttendanceHistory.vue
@@ -111,7 +111,7 @@
               Swal.fire({
                     icon: 'success',
                     title: 'Datos registrados',
-                    text: 'Se ha registrado con exito.',
+                    text: 'Se ha registrado la entrada con éxito.',
                 });
             }).catch(error => {
                     Swal.fire({
