b2b5e4b25be762410a3583078d45120db583aa63
LuisVicente123 <luisarandavi@gmail.com>
2021-12-06 13:06:39 -0600

    corregir erroreS2

--- resources/js/components/index.vue
+++ resources/js/components/index.vue
@@ -12,32 +12,6 @@
           </div>
           <div class="col-lg-9 col-md-9 col-sm-12">
           <div class="row" style="padding-top:50px">
-            <div class="col-lg-12">
-              
-                <div class="row" style="min-height: 250px; border-radius: 20px;width: 65%;border: 10px solid white; background-color: white;position: relative;left: 650px;">
-                  <div class="col-lg-12" method="post" >
-                    <h2>Inicio de sesion</h2>
-                    </div>
-                  <div class="col-lg-12">
-                    <fieldset>
-                    <label>Usuario:</label>
-                        <input type="text">
-                        </fieldset>
-                  </div>
-                  <div class="col-lg-12">
-                    <fieldset>
-                     <label>Contraseña:</label>
-                        <input type="password">
-                        </fieldset>
-                      </div>
-                 <div class="col-lg-12">
-                    <fieldset>
-                      <button type="submit" style="border-radius:20px;" id="form-submit" class="btn btn-primary" >Registrar</button>
-                    </fieldset>
-                  </div>
-                </div>
-             
-            </div>
           </div>
         </div>
               </div>
