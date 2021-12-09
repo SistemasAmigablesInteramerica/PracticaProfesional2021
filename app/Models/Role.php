bc27e6eb6efebff0ee428c0d20ca94d2d140fe57
TheKnightOfDarkness <gameroomca@gmail.com>
2021-12-09 05:33:30 -0600

    nuevo

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -21,6 +21,6 @@ class Role extends Model
     }
     public function permissions()
     {
-        return $this->belongsToMany(Permission::class,'permission_role', 'role_id', 'permission_id');
+        return $this->belongsToMany(Permission::class,'permission_roles', 'role_id', 'permission_id');
     }
 }
