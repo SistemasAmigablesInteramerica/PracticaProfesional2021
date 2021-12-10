71d5f9f9e075c9365c308bf96455265a02cc7a18
TheKnightOfDarkness <gameroomca@gmail.com>
2021-12-10 02:47:02 -0600

    nuevo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(PermissionDataTableSeeder::class);
+
+        $this->call(UserDataTableSeeder::class);
     }
 }
